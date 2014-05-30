09f6895bf58ffa6ba581d7f045a310d70a104ab9
Joshua J. Berk <joshua.j.berk@gmail.com>
2014-05-30 14:13:20 -0400

    altered mission control/notification center

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -340,7 +340,7 @@ defaults write com.apple.dock showhidden -bool true
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tl-modifier -int 0
 # Top right screen corner → Application Windows
-defaults write com.apple.dock wvous-tr-corner -int 3
+defaults write com.apple.dock wvous-tr-corner -int 12
 defaults write com.apple.dock wvous-tr-modifier -int 0
 # Bottom left screen corner → Launchpad
 defaults write com.apple.dock wvous-bl-corner -int 11
