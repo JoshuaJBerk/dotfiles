62a9c7ac2f808948a9fa95b1a6d1d4632a9a6a7f
Joshua J. Berk <joshua.j.berk@gmail.com>
2014-07-17 09:10:59 -0400

    Dispatch + Repeat + Hardtime

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,14 +2,12 @@
 
 if test ! $(which rbenv)
 then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+  brew install rbenv
 fi
 
 if test ! $(which ruby-build)
 then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+  brew install ruby-build
 fi
 
 # Update System Gems
