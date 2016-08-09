ab9b403ecfdfc1e4947054d6bcabb76cede085d0
Joshua Berk <joshua.j.berk@gmail.com>
2016-08-09 09:23:38 -0500

    remove modifier key apple script

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -154,25 +154,6 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
 # Stop iTunes from responding to the keyboard media keys
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 
-# Map Caps Lock Modifier Key to Control
-/usr/bin/osascript > /dev/null << EOT
-tell application "System Preferences"
-    reveal anchor "keyboardTab" of pane "com.apple.preference.keyboard"
-end tell
-tell application "System Events" to tell window 1 of process "System Preferences"
-    click button 1 of tab group 1
-    tell sheet 1
-        tell pop up button 4
-            click
-            delay 0.1
-                click menu item 2 of menu 1
-        end tell
-        click button "OK"
-    end tell
-end tell
-quit application "System Preferences"
-EOT
-
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
