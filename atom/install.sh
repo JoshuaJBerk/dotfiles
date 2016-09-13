3eb3c2eec4c4857807d4c2b1c8ff31430eea3e02
Joshua Berk <joshua.j.berk@gmail.com>
2016-09-13 16:55:13 -0500

    hello atom

---
+++ atom/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Install atom packages
+
+# Git
+apm install git-plus
+apm install git-time-machine
+
+# Linter
+apm install linter
+
+# Design
+apm install pigments
+
+# REPL
+apm install atom-html-preview
+
+# Miscellaneous
+apm install file-icons
+apm install minimap
+apm install atom-beautify
+apm install vim-mode
